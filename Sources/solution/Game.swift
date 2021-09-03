fb8abc5db2c66ae32302e99870afce94305867dd
Mark Johnson <markj@matzsoft.com>
2021-09-02 20:12:52 -0700

    Added debug mode to the meta commands.
    1. Allows breakpoints.
    2. Allows access to ip, registers, and memory.
    3. Moved trace and disassemble to debug mode.
    4. Added file name argument to save and restore.

--- Sources/solution/Game.swift
+++ Sources/solution/Game.swift
@@ -14,6 +14,7 @@ struct Game {
     var inputQueue:  [String]
     var trace:       Bool
     var traceBuffer: [String]
+    var breakpoints: Set<Int>
     
     var isHalted:    Bool { computer.halted }
     
@@ -39,6 +40,7 @@ struct Game {
         inputQueue  = []
         trace       = false
         traceBuffer = []
+        breakpoints = Set()
     }
     
     init( from other: Game ) {
@@ -48,6 +50,7 @@ struct Game {
         inputQueue  = other.inputQueue
         trace       = other.trace
         traceBuffer = other.traceBuffer
+        breakpoints = other.breakpoints
     }
 
     func command( value: String ) -> Void {
@@ -64,6 +67,10 @@ struct Game {
                 }
             }
 
+            if breakpoints.contains( computer.ip ) {
+                print( "Breakpoint at \( try computer.disassemble( address: computer.ip ) )" )
+                try debugMode()
+            }
             if trace { try traceBuffer.append( computer.trace() ) }
             if let output = try computer.step(), output.isASCII {
                 outputQueue.append( output )
@@ -118,53 +125,102 @@ struct Game {
         case "save":
             let encoder = JSONEncoder()
             let json = try encoder.encode( computer )
+            let file = words.count < 2 ? "challenge.json" : "\(words[1]).json"
             
-            try json.write( to: URL( fileURLWithPath: "challenge.json" ) )
+            try json.write( to: URL( fileURLWithPath: file ) )
             print( prompt )
         case "restore":
             let decoder = JSONDecoder()
-            let json = try Data( contentsOf: URL( fileURLWithPath: "challenge.json" ) )
+            let file = words.count < 2 ? "challenge.json" : "\(words[1]).json"
+            let json = try Data( contentsOf: URL( fileURLWithPath: file ) )
             
             computer = try decoder.decode( SynacorCode.self, from: json )
             command( value: "look" )
         case "restart":
             computer = try Game.initialComputer( from: fileURL )
-        case "trace":
-            if words.count == 1 {
-                trace = !trace
-            } else {
-                switch words[1] {
-                case "on":
-                    trace = true
-                case "off":
-                    trace = false
-                case "clear":
-                    traceBuffer = []
-                default:
-                    try traceBuffer.joined( separator: "\n" ).write(
-                        toFile: "\(words[1]).trace", atomically: true, encoding: .utf8 )
-                }
-            }
-            print( "Trace mode is now \( trace ? "on" : "off" )." )
-            print( prompt )
-        case "r7":
-            if words.count > 1 {
-                computer.registers[7] = UInt16( words[1] )!
-            }
-            print( "The eighth register is \( computer.registers[7] )." )
-            print( prompt )
-        case "disassemble":
-            let start = words.count < 2 ? 0 : Int( words[1] )!
-            let file = words.count < 3 ? "default.asm" : "\(words[2]).asm"
-            let results = try computer.disassembler( address: start ).joined( separator: "\n" )
-            
-            try results.write( toFile: file, atomically: true, encoding: .utf8 )
+        case "debug":
+            try debugMode()
             print( prompt )
         case "die":
             print( "You do your best grue mating call and are soon eaten by a pack of angry grues." )
             computer.halted = true
         default:
             command( value: line )
+        }
+    }
+    
+    mutating func debugMode() throws -> Void {
+        let prompt = "debug command?"
+        
+        LOOP:
+        while true {
+            print( prompt )
+
+            let line = readLine() ?? ""
+            let words = line.split( separator: " " )
+            
+            guard !words.isEmpty else { continue }
+            switch words[0] {
+            case "b":
+                if words.count > 1 {
+                    breakpoints.insert( Int( words[1] )! )
+                } else {
+                    print( "Breakpoints:" )
+                    try breakpoints.map { try computer.disassemble( address: $0 ) }.forEach {
+                        print("   \($0)" )
+                    }
+                }
+            case "B":
+                if words.count > 1 {
+                    breakpoints.remove( Int( words[1] )! )
+                } else {
+                    print( "Breakpoints:", breakpoints.map { String( $0 ) }.joined( separator: ", " ) )
+                }
+            case "ip":
+                if words.count > 1 {
+                    computer.ip = Int( words[1] )!
+                }
+                print( "ip:", computer.ip )
+            case "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7":
+                let register = Int( words[0].dropFirst() )!
+                if words.count > 1 {
+                    computer.registers[register] = UInt16( words[1] )!
+                }
+                print( words[0], "=", computer.registers[register] )
+            case let address where Int( address ) != nil:
+                let address = Int( address )!
+                if words.count > 1 {
+                    computer.memory[address] = UInt16( words[1] )!
+                }
+                print( "Memory at", address, "contains", computer.memory[address] )
+            case "trace":
+                if words.count == 1 {
+                    trace = !trace
+                } else {
+                    switch words[1] {
+                    case "on":
+                        trace = true
+                    case "off":
+                        trace = false
+                    case "clear":
+                        traceBuffer = []
+                    default:
+                        try traceBuffer.joined( separator: "\n" ).write(
+                            toFile: "\(words[1]).trace", atomically: true, encoding: .utf8 )
+                    }
+                }
+                print( "Trace mode is now \( trace ? "on" : "off" )." )
+            case "disassemble":
+                let start = words.count < 2 ? 0 : Int( words[1] )!
+                let file = words.count < 3 ? "challenge.asm" : "\(words[2]).asm"
+                let results = try computer.disassembler( address: start ).joined( separator: "\n" )
+                
+                try results.write( toFile: file, atomically: true, encoding: .utf8 )
+            case "go":
+                break LOOP
+            default:
+                break
+            }
         }
     }
 }
